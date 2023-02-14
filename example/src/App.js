7bfe01fbe50b8483f9f7521e5ab62ef51fb7a716
aarathyKeyvalue <aarathy@keyvalue.systems>
2023-02-14 11:00:07 +0530

    example folder

---
+++ example/src/App.js
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import VerticalStepper from 'vertical-stepper';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    const verticalStepperInstance = new VerticalStepper();
+    verticalStepperInstance.myMethod();
+  }, []);
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
