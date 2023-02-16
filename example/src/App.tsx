2ac51b37185297ea12b762054ac25215255b2ef8
GokulSNath <gokul.s@keyvalue.systems>
2023-02-16 10:52:22 +0530

    changes to example project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,9 @@ import './App.css';
 import MyLibrary from "vertical-stepper";
 function App() {
   return (
+    <div style={{ width:'fit-content', height:'50vh', paddingTop:'10vh', paddingLeft:'10vh'}}>
     <MyLibrary />
+    </div>
   );
 }
 
